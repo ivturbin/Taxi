833b6a50a2d2881a04a41d34259682c9bda27f7b
rgmusaev <27121997Rr!>
2021-09-20 18:55:29 +0300

    init commit

---
+++ src/main/java/ru/digitalleague/mytaxiservice/MyTaxiServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.digitalleague.mytaxiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTaxiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTaxiServiceApplication.class, args);
+    }
+
+}
